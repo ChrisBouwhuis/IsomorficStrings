1c954c004ec9a344c1b4a705bd838de6d18fe1de
Chris Bouwhuis <chrisb190203@gmail.com>
2024-04-02 16:29:13 +0200

    Add project files.

---
+++ ClassLib/Class1.cs
@@ -0,0 +1,38 @@
+﻿namespace ClassLib
+{
+    public class Checker
+    {
+        public bool IsIsometric(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, char> mapS = new Dictionary<char, char>();
+            Dictionary<char, char> mapT = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!mapS.ContainsKey(s[i]))
+                {
+                    mapS.Add(s[i], t[i]);
+                }
+                else if (mapS[s[i]] != t[i])
+                {
+                    return false;
+                }
+
+                if (!mapT.ContainsKey(t[i]))
+                {
+                    mapT.Add(t[i], s[i]);
+                }
+                else if (mapT[t[i]] != s[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ IsometricStringChecker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLib;
+
+namespace IsometricStringChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var isometricStringChecker = new Checker();
+            bool result = isometricStringChecker.IsIsometric("egg", "add");
+            Console.WriteLine(result);
+        }
+    }
+}
