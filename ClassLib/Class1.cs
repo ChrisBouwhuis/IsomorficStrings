3c46b6907009a05db0e14d97a7352b167dd24b2a
Chris Bouwhuis <60960815+ChrisBouwhuis@users.noreply.github.com>
2024-04-02 16:46:14 +0200

    Update Class1.cs

--- ClassLib/Class1.cs
+++ ClassLib/Class1.cs
@@ -4,35 +4,24 @@
     {
         public bool IsIsometric(string s, string t)
         {
-            if (s.Length != t.Length)
+         var dictionary = new Dictionary<char, char>();
+         for (var i = 0; i < s.Length; i++)
+        {
+            if (dictionary.TryGetValue(s[i], out var currentPair))
+            {
+                if (currentPair != t[i])
+                {
+                    return false;
+                }
+                continue;
+            }
+            if (dictionary.ContainsValue(t[i]))
             {
                 return false;
             }
-
-            Dictionary<char, char> mapS = new Dictionary<char, char>();
-            Dictionary<char, char> mapT = new Dictionary<char, char>();
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (!mapS.ContainsKey(s[i]))
-                {
-                    mapS.Add(s[i], t[i]);
-                }
-                else if (mapS[s[i]] != t[i])
-                {
-                    return false;
-                }
-
-                if (!mapT.ContainsKey(t[i]))
-                {
-                    mapT.Add(t[i], s[i]);
-                }
-                else if (mapT[t[i]] != s[i])
-                {
-                    return false;
-                }
-            }
-            return true;
+            dictionary.Add(s[i], t[i]);
+        }
+        return true;
         }
     }
 }
